2826df2a47593f6e71c020f638009bca3465d251
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-08-07 11:23:54 -0700

    Finished adding average rating functionality

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ const ratingText = document.querySelector('#rating-text')
 const radioButtons = document.querySelectorAll('input[type="radio"]')
 const stars = document.querySelectorAll(".fa-star")
 let rating
-// let averageRating = 4
-let sumOfRatings 
-let totalRatings 
-let averageRating 
+let averageRating
 
-// console.log(sumOfRatings, totalRatings, totalRatings)
-
+// sets counts for sum of ratings and total rating count to calculate average later
+let sumOfRatings = parseInt(window.localStorage.getItem("sumOfRatings")) || 0
+let totalRatings = parseInt(window.localStorage.getItem("totalRatings")) || 0
 
 radioButtons.forEach(btn => {
   btn.checked = false
@@ -23,9 +21,12 @@ function updateRating(e) {
   rating = parseInt(e.target.value);
 }
 
+// dynamically changes average rating display
 function handleAverage(newRating) {
     [...stars].map((e,i) =>{
+        //makes all stars outlines
         e.classList.remove("fa-solid", "fa-beat")
+        //changes outlines to fills depending on average
         i < averageRating ? e.classList.add("fa-solid", "fa-beat") : null
     })
 }
@@ -33,33 +34,26 @@ function handleAverage(newRating) {
 function handleSubmit(e) { 
   e.preventDefault()
   if (rating != undefined) {
-    // window.localStorage.setItem("Average Rating", Math.floor((averageRating + rating)/2))
-    // averageRating = Math.floor((averageRating + rating)/2)
-
-    // let averageRating = sumOfRatings / totalRatings
-
-    // let sumOfRatings = parseInt(window.localStorage.getItem("sum of ratings"))
-    // let totalRatings = parseInt(window.localStorage.getItem("total ratings"))
-    // window.localStorage.setItem("sum of ratings", sumOfRatings + rating)
-    // window.localStorage.setItem("total ratings", totalRatings++)
-    window.localStorage.setItem("sum of ratings", sumOfRatings)
+    // updates sum of ratings in local storage
+    window.localStorage.setItem("sumOfRatings", (sumOfRatings + rating))
+    sumOfRatings = sumOfRatings + rating
     
-    averageRating = Math.floor((averageRating + rating))
+    // updates total ratings in local storage
+    window.localStorage.setItem("totalRatings", totalRatings + 1)
+    totalRatings += 1
 
-    // averageRating = Math.floor((averageRating + rating)/2)
-
+    // updates average rating variable and display
+    averageRating = Math.floor(sumOfRatings / totalRatings)
     handleAverage(rating)
+
+    // changes display from rating panel to thank you panel
     ratingState.style.display = "none"
     thankYouState.style.display = "flex"
     ratingText.innerHTML = `You selected ${rating} out of 5`
-
-    // console.log(window.localStorage.getItem("sum of ratings"))
-    // console.log(window.localStorage.getItem("total ratings"))
-    console.log(sumOfRatings, totalRatings, totalRatings)
-    
   }
 }
 
+// handles back button
 function back() {
     ratingState.style.display = "contents"
     thankYouState.style.display = "none"
